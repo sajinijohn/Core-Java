cf372a081f8548f07f8e1901da07e82c151c1014
sajinijohn <58158426+sajinijohn@users.noreply.github.com>
2019-12-11 10:47:11 +0530

    static variable

---
+++ Staticexamaple.java
@@ -0,0 +1,17 @@
+package staticexp;
+
+public class Staticexamaple {
+static int count=0;
+Staticexamaple()
+{
+	count++;
+	System.out.println(count);
+	}
+public static void main(String[]args)
+{
+	Staticexamaple c1=new Staticexamaple();
+	Staticexamaple c2=new Staticexamaple();
+	Staticexamaple c3=new Staticexamaple();
+	
+	}
+}
