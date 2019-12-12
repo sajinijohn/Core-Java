5117c09ed1ae325c2fea8646c3bd6e3b19db63fa
sajinijohn <58158426+sajinijohn@users.noreply.github.com>
2019-12-12 12:56:42 +0530

    class using getter and setter methods

--- class32.java
+++ class32.java
@@ -1,58 +1,58 @@
 package corejava;
-
-public class class32 {
+public class class32 
+ {
 	String name;
-	 String address;
+	String address;
 	String ph;
 	String email;
 	String ptype;
 	String pid;
-	public void setname(String name) 
-	{
+	public void setname(String name) //set method to update the values
+	 {
 		this.name=name;
-	}
+	 }
 	public void setadd(String address) 
-	{
+	 {
 		this.address=address;
-	}
+	 }
 	public void setphone(String ph) 
-	{
+	 {
 		this.ph=ph;
-	}
+	 }
 	public void setemail(String email) 
-	{
+	 {
 		this.email=email;
-	}
+	 }
 	public void setprooft(String ptype) 
-	{
+	 {
 		this.ptype=ptype;
-	}
+	 }
 	public void setproofid(String pid) 
-	{
+	 {
 		this.pid=pid;
-	}
-	public String getname() 
-	{
+	 }
+	public String getname() //get method to retrive values
+	 {
 		return this.name;
-	}
+	 }
 	public String getadd() 
-	{
+	 {
 		return this.address;
-	}
+	 }
 	public String getphone() 
-	{
+	 {
 		return this.ph;
-	}
+	 }
 	public String getemail() 
-	{
+	 {
 		return this.email;
-	}
+	 }
 	public String getprooft() 
-	{
+	 {
 		return this.ptype;
-	}
+	 }
 	public String getproofid() 
-	{
+	 {
 		return this.pid;
-	}
+	 }
 }
