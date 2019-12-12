d87b57a3124295bbaf2fb1134067a38e378838ba
sajinijohn <58158426+sajinijohn@users.noreply.github.com>
2019-12-12 14:02:25 +0530

    Interface example

--- interfaceeg.java
+++ interfaceeg.java
@@ -1,29 +1,30 @@
 package corejava;
-interface IT1
-{
-	void method1();
+interface IT1//interface
+ {
+	void method1();//abstract method
 	void method2();
 	void method3();
-	}
+ }
 
-public class interfaceeg implements IT1 {
+public class interfaceeg implements IT1 
+ {
 	public void method1()
-	{
-		System.out.println("Method 1");
-	}
+	 {
+	   System.out.println("Method 1");
+	 }
 	public void method2()
-	{
-		System.out.println("Method 2");
-	}
+	 {
+	   System.out.println("Method 2");
+	 }
 	public void method3()
-	{
-		System.out.println("Method 3");
-	}
+	 {
+	   System.out.println("Method 3");
+	 }
 public static void main(String[]args)
-{
+ {
 	IT1 T=new interfaceeg();
 	T.method1();
 	T.method2();
 	T.method3();
-	}
+ }
 }
