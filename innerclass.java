8e7cf67a7e428631ab8d2072488d64619a15308b
sajinijohn <58158426+sajinijohn@users.noreply.github.com>
2019-12-12 13:59:34 +0530

    Inner class example

--- innerclass.java
+++ innerclass.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 class innerclass
 {
 	private int a=10;
-	class innerclass1
-	{
+	class innerclass1//inner class
+	 {
 		public void display()
-		{
+		 {
 			System.out.println(a);
-		}	
-	}
+		 }	
+	 }
 	public static void main(String[]args)
 	{
-		innerclass outer=new innerclass();
-		innerclass.innerclass1 ob=outer.new innerclass1();
+		innerclass outer=new innerclass();//creating object for outer class
+		innerclass.innerclass1 ob=outer.new innerclass1();//creating object for inner class
 		ob.display();
 	}
 }
