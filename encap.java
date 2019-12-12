75e8c870a41399daca73ead3f43e9c45a60023ed
sajinijohn <58158426+sajinijohn@users.noreply.github.com>
2019-12-12 12:59:35 +0530

    encapsulation class

--- encap.java
+++ encap.java
@@ -1,58 +1,58 @@
 package corejava;
-
-public class encap {
+public class encap
+ {
 	String name;
-	 String address;
+	String address;
 	String ph;
 	String email;
 	String ptype;
 	String pid;
-	public void setname(String name) 
-	{
+	public void setname(String name) //set method to update values
+	 {
 		this.name=name;
-	}
+	 }
 	public void setadd(String address) 
-	{
+	 {
 		this.address=address;
-	}
+	 }
 	public void setphone(String ph) 
-	{
+	 {
 		this.ph=ph;
-	}
+	 }
 	public void setemail(String email) 
-	{
+	 {
 		this.email=email;
-	}
+	 }
 	public void setprooft(String ptype) 
-	{
+	 {
 		this.ptype=ptype;
-	}
+	 }
 	public void setproofid(String pid) 
-	{
+	 {
 		this.pid=pid;
-	}
-	public String getname() 
-	{
+	 }
+	public String getname() //get method to retrive the values
+	 {
 		return this.name;
-	}
+	 }
 	public String getadd() 
-	{
+	 {
 		return this.address;
-	}
+	 }
 	public String getphone() 
-	{
+	 {
 		return this.ph;
-	}
+	 }
 	public String getemail() 
-	{
+	 {
 		return this.email;
-	}
+	 }
 	public String getprooft() 
-	{
+	 {
 		return this.ptype;
-	}
+	 }
 	public String getproofid() 
-	{
+	 {
 		return this.pid;
-	}
+	 }
 }
