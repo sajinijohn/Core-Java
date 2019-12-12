61daa52456fd70224e31af841a48dfbb6acae10d
sajinijohn <58158426+sajinijohn@users.noreply.github.com>
2019-12-12 12:22:37 +0530

    Abstraction example

--- abstraction.java
+++ abstraction.java
@@ -1,34 +1,34 @@
 package corejava;
-abstract class Animal
+abstract class Animal//abstract class
 {
 	private String name;
 	public Animal(String name)
 	{
 		this.name=name;
 	}
-	public abstract void eat();
-	public abstract void breathe();
+	public abstract void eat();//abstract method
+	public abstract void breathe();//abstract method
 	public String getname()
 	{
 		return name;
 	}
-	}
-class Racoon extends Animal
+ }
+class Racoon extends Animal//Inherited class
 {
 	public Racoon(String name)
 	{
 		super(name);
-		}
-public void eat()
-{
+	}
+        public void eat()//abstract class definition
+        {
 	System.out.println(getname()+" is eating");
 	}
-public void breathe()
-{
+        public void breathe()//abstract class definition
+        {
 	System.out.println("Breathe in,braethe out,repeat");
 	}
 
-	}
+}
 public class abstraction {
 	public static void main(String[]args)
 	{
@@ -36,5 +36,4 @@ public class abstraction {
 		racoon.eat();
 		racoon.breathe();
 	}
-
 }
