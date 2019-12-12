3a7cae54ab8961cf5d3839656c87e26ffdd54a29
sajinijohn <58158426+sajinijohn@users.noreply.github.com>
2019-12-12 12:17:45 +0530

    static keyword example

--- Staticexamaple.java
+++ Staticexamaple.java
@@ -1,17 +1,17 @@
 package staticexp;
-
-public class Staticexamaple {
-static int count=0;
-Staticexamaple()
+public class Staticexamaple 
 {
-	count++;
-	System.out.println(count);
-	}
-public static void main(String[]args)
-{
-	Staticexamaple c1=new Staticexamaple();
-	Staticexamaple c2=new Staticexamaple();
-	Staticexamaple c3=new Staticexamaple();
+  static int count=0;//static variable
+  Staticexamaple()
+  {
+     count++;
+     System.out.println(count);
+  }
+ public static void main(String[]args)
+  {
+	Staticexamaple c1=new Staticexamaple();//first object created
+	Staticexamaple c2=new Staticexamaple();//second object created
+	Staticexamaple c3=new Staticexamaple();///third object created
 	
-	}
+  }
 }
