766ddcc0c91bdf100035d3f9bc84b470c8c259c8
sajinijohn <58158426+sajinijohn@users.noreply.github.com>
2019-12-12 12:26:45 +0530

    anonymous class exmaple

--- anony.java
+++ anony.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
-abstract class anonym
-{
-	public abstract void learn();
-	}
-public class anony {
-	public static void main(String[]args)
-	{
-		anonym ob=new anonym()
-		{
-		 public void learn()
-		{
-			System.out.println("welcome to ddu-gky");
-		}
-		};
-		ob.learn();
-	}
+abstract class anonym//abstract class
+ {
+  public abstract void learn();//abstract method
+ }
+ public class anony
+  {
+public static void main(String[]args)
+ {
+   anonym ob=new anonym()//anonymous class
+     {
+	 public void learn()
+	  {
+	    System.out.println("welcome to ddu-gky");
+	   }
+     };//anonymous class ends
+ ob.learn();
+  }
 }
