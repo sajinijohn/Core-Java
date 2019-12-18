3559feb7bf4c7f0352cdf46190db96e41f0771e9
sajinijohn <58158426+sajinijohn@users.noreply.github.com>
2019-12-18 11:59:09 +0530

    String Builderclass example

--- Stringbuilderexample.java
+++ Stringbuilderexample.java
@@ -3,18 +3,18 @@ public class Stringbuilderexample
  {
    public static void main(String[]args)
     { 
-	   StringBuilder s1=new StringBuilder("Hello");
-	   StringBuilder s2=new StringBuilder("World");
+	   StringBuilder s1=new StringBuilder("Hello");//string1 is declared
+	   StringBuilder s2=new StringBuilder("World");//string2 is declared
 	   System.out.println(s1);
 	   System.out.println(s2);
-	   System.out.println(s1.hashCode());
-	   System.out.println(s2.hashCode());
+	   System.out.println(s1.hashCode());//printing hash code of string1
+	   System.out.println(s2.hashCode());//printing hash code of string2
 	   System.out.println(s1);
 	   System.out.println(s2);
-	   System.out.println(s1.append(s2));
+	   System.out.println(s1.append(s2));//appending string2 to string1
 	   System.out.println(s1);
 	   System.out.println(s2);
-	   System.out.println(s1.hashCode());
+	   System.out.println(s1.hashCode());//new hash code will be same as old using builder function
 	   System.out.println(s2.hashCode());
     }
  }
